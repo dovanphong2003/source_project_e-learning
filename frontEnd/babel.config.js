71a2a0b7e4def4dcf742f69c8333a1d99a2c34db
do van phong <dodaiphong299@gmail.com>
2023-07-27 15:10:08 +0700

    upload source code

---
+++ frontEnd/babel.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    presets: [
+        // Đặt các preset Babel của bạn ở đây (nếu có)
+    ],
+    plugins: [
+        // Thêm @babel/plugin-syntax-dynamic-import vào đây
+        "@babel/plugin-syntax-dynamic-import",
+        // Đặt các plugin Babel khác của bạn ở đây (nếu có)
+    ],
+};
