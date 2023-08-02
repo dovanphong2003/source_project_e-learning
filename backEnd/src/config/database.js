09d85631a1da766bfafaaf92906d7446a2bab59b
do van phong <dodaiphong299@gmail.com>
2023-08-02 15:50:54 +0700

    thay doi nho

--- backEnd/src/config/database.js
+++ backEnd/src/config/database.js
@@ -5,7 +5,7 @@ const pool = new Pool({
     host: process.env.LOCALHOST_CONNECT_DATABASE,
     user: "postgres", // Thay đổi nếu bạn sử dụng tên người dùng khác.
     max: 20,
-    port: process.env.PORT_DATABASE_CONNECT, // Sử dụng giá trị từ biến môi trường.
+    port: 5433, // Sử dụng giá trị từ biến môi trường.
     password: process.env.DATABASE_PASSWORD, // Sử dụng giá trị từ biến môi trường.
     database: process.env.DATABASE_NAME, // Sử dụng giá trị từ biến môi trường.
     idleTimeoutMillis: 30000,
@@ -18,8 +18,34 @@ const pool = new Pool({
 pool.connect((err) => {
     if (err) {
         console.error("Error connecting to database:", err.message);
+        console.error("info maition:", {
+            host: process.env.LOCALHOST_CONNECT_DATABASE,
+            user: "postgres", // Thay đổi nếu bạn sử dụng tên người dùng khác.
+            max: 20,
+            port: 5433, // Sử dụng giá trị từ biến môi trường.
+            password: process.env.DATABASE_PASSWORD, // Sử dụng giá trị từ biến môi trường.
+            database: process.env.DATABASE_NAME, // Sử dụng giá trị từ biến môi trường.
+            idleTimeoutMillis: 30000,
+            connectionTimeoutMillis: 4000,
+            ssl: {
+                rejectUnauthorized: false,
+            },
+        });
         return;
     }
+    console.error("info maition:", {
+        host: process.env.LOCALHOST_CONNECT_DATABASE,
+        user: "postgres", // Thay đổi nếu bạn sử dụng tên người dùng khác.
+        max: 20,
+        port: 5433, // Sử dụng giá trị từ biến môi trường.
+        password: process.env.DATABASE_PASSWORD, // Sử dụng giá trị từ biến môi trường.
+        database: process.env.DATABASE_NAME, // Sử dụng giá trị từ biến môi trường.
+        idleTimeoutMillis: 30000,
+        connectionTimeoutMillis: 4000,
+        ssl: {
+            rejectUnauthorized: false,
+        },
+    });
     console.log("Database connected!");
 });
 
