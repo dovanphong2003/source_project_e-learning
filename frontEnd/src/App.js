641ef5c640e341e0ce5c949a1ae6b5aba5a1205d
do van phong <dodaiphong299@gmail.com>
2023-08-12 12:20:41 +0700

    fix error set role for user

--- frontEnd/src/App.js
+++ frontEnd/src/App.js
@@ -2,13 +2,59 @@ import React, { useEffect, useState } from "react";
 import { DisplayUser } from "./assets/display/DisplayUser";
 import { DisplayAdmin } from "./assets/display/DisplayAdmin";
 import AutoScrollTop from "./components/Sections/AutoScrollTop";
+import axios from "axios";
+import { VerifyToken } from "./components/Sections/FunctionAll";
 const App = function () {
     const [roleUser, setRoleUser] = useState("student");
+
+    //////////////////////////
+    // get info user - id
+    const fncgetInfoUserByAccessTokenAPI = async () => {
+        try {
+            const response = await axios.get(
+                `${
+                    process.env.REACT_APP_URL_BACKEND
+                }/getInfoUserByAccessTokenAPI?accessToken=${localStorage.getItem(
+                    "accessToken"
+                )}`
+            );
+            console.log("data user: ", response.data.data);
+            localStorage.setItem("role", response.data.data.role);
+            setRoleUser(response.data.data.role);
+            return response.data.data;
+        } catch (error) {
+            if (error.response.data.ec.message === "jwt expired") {
+                const funcVerifyToken = await VerifyToken();
+                const checkVerify = await funcVerifyToken();
+                if (checkVerify) {
+                    try {
+                        const response = await axios.get(
+                            `${
+                                process.env.REACT_APP_URL_BACKEND
+                            }/getInfoUserByAccessTokenAPI?accessToken=${localStorage.getItem(
+                                "accessToken"
+                            )}`
+                        );
+                        console.log("data user: ", response.data.data);
+                        localStorage.setItem("role", response.data.data.role);
+                        setRoleUser(response.data.data.role);
+                        return response.data.data;
+                    } catch (error) {}
+                }
+            }
+        }
+    };
     useEffect(() => {
-        localStorage.getItem("role")
-            ? setRoleUser(localStorage.getItem("role"))
-            : setRoleUser("student");
+        console.log("da vao den day");
+        if (
+            localStorage.getItem("accessToken") &&
+            localStorage.getItem("role") &&
+            localStorage.getItem("role") !== "virtualUser"
+        ) {
+            fncgetInfoUserByAccessTokenAPI();
+        }
     }, [localStorage.getItem("role")]);
+    ////////////////////////////
     return (localStorage.getItem("role") &&
         localStorage.getItem("role") === "admin") ||
         roleUser === "admin" ? (
